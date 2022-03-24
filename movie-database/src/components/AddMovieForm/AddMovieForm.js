5180645dad22d7322713cfcad21d28906e13a71f
AisyahMutmainnah <73178425+AisyahMutmainnah@users.noreply.github.com>
2022-03-24 11:36:31 +0700

    Update AddMovieForm.js

--- movie-database/src/components/AddMovieForm/AddMovieForm.js
+++ movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -4,22 +4,27 @@ import styles from "./AddMovieForm.module.css";
 
 function AddMovieForm() { 
     return(
+        
         <div className={styles.container}>
             <section className={styles.AddMovieForm}>
+                
                 <div className={styles.AddMovieForm__left}>
                     <img className={styles.AddMovieForm__image} 
-                    src="https://picsum.photos/400/300" alt="placeholder"/> 
+                    src="https://picsum.photos/400/300" alt=""/> 
                 </div> 
+
                 <div className={styles.AddMovieForm__right}>
+                    
                     <h2 className={styles.AddMovieForm__title}>Add Movie</h2> 
                     <form className={styles.AddMovieForm__form}>
                             <label className={styles.AddMovieForm__label} htmlFor="">Title</label> 
                             <br/>
-                            <input className={styles.AddMovieForm__input} type="text"/> 
+                            <input className={styles.AddMovieForm__inputtitle} type="text"/> 
                             <br/>
+                            <br/>    
                             <label className={styles.AddMovieForm__label} htmlFor="">Year</label> 
                             <br/>
-                            <input className={styles.AddMovieForm__input} type="text"/> 
+                            <input className={styles.AddMovieForm__inputyear} type="text"/> 
                             <br />
                             <button className={styles.AddMovieForm__button}>Submit</button> 
                     </form>
